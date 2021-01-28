a79f12f013369620cce2dac6824ff3879f9328fd
malike <st.malike@gmail.com>
2021-01-28 22:18:24 +0000

    added mongo dependency and refactored port for the service admin

--- service-admin/src/main/java/st/malike/manifest/model/Data.java
+++ service-admin/src/main/java/st/malike/manifest/model/Data.java
@@ -1,6 +1,11 @@
 package st.malike.manifest.model;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * @author malike_st
@@ -19,6 +24,51 @@ public class Data implements Serializable {
     @Indexed
     private Date dateCreated;
 
-    
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
 }
