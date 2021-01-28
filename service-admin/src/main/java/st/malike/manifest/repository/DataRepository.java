1a5a72148c298629e0b4bfa3ebdf0f02e461dc63
malike <st.malike@gmail.com>
2021-01-28 23:02:19 +0000

    service admin set up

--- service-admin/src/main/java/st/malike/manifest/repository/DataRepository.java
+++ service-admin/src/main/java/st/malike/manifest/repository/DataRepository.java
@@ -1,9 +1,12 @@
 package st.malike.manifest.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+import st.malike.manifest.model.Data;
+
 /**
  * @author malike_st
  */
-public interface DataRepository {
+public interface DataRepository extends MongoRepository<Data, String> {
 
 }
 
--- service-admin/src/main/java/st/malike/manifest/service/DataService.java
+++ service-admin/src/main/java/st/malike/manifest/service/DataService.java
@@ -1,11 +1,19 @@
 package st.malike.manifest.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+import st.malike.manifest.repository.DataRepository;
 
 /**
  * @author malike_st
  */
 @Service
 public class DataService {
+
+  @Autowired
+  private DataRepository dataRepository;
+  @Autowired
+  private MongoTemplate mongoTemplate;
 
 }
